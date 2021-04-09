023910ff9f247cd9b5a432908a432dc4e1d046fe
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-04-09 10:36:29 -0300

    Completed exercises 3.7. and 3.8.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express")
+const morgan = require("morgan")
 
 const app = express()
 
 app.use(express.json())
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :postData"))
+
+morgan.token("postData", function (request, response) {
+    return JSON.stringify(request.body)
+})
 
 let persons = [
     {
